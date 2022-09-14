059ed9d8ffd5f8b88f09e312968d138f2af0f176
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-14 01:41:21 -0300

    chore: add collect coverage folders

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/domain/**', '!<rootDir>/src/main/**', '!<rootDir>/src/**/**-protocols.ts', '!<rootDir>/src/**/index.ts'],
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
