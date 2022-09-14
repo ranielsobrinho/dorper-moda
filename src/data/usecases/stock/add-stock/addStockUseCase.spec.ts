780da1da2fa45b53c7cd42be8131655751936628
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-14 01:47:33 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/stock/add-stock/addStockUseCase.spec.ts
+++ src/data/usecases/stock/add-stock/addStockUseCase.spec.ts
@@ -1,25 +1,40 @@
 import { AddStockRepository } from '../../../protocols/db/stock/addStockRepository'
 import { AddStockUseCase } from './addStockUseCase'
 
+const makeStockDataRequest = () => ({
+  modelName: 'any_name',
+  color: 'any_color',
+  quantity: 1,
+})
+
+const makeFakeAddStockRepositoryStub = (): AddStockRepository => {
+  class AddStockRepositoryStub implements AddStockRepository {
+    async add({}): Promise<void> {
+      return
+    }
+  }
+  return new AddStockRepositoryStub()
+}
+
+type SutTypes = {
+  sut: AddStockUseCase
+  addStockRepositoryStub: AddStockRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addStockRepositoryStub = makeFakeAddStockRepositoryStub()
+  const sut = new AddStockUseCase(addStockRepositoryStub)
+  return {
+    sut,
+    addStockRepositoryStub,
+  }
+}
+
 describe('AddStockUseCase', () => {
   test('Should call AddStockRepository with correct values', async () => {
-    class AddStockRepositoryStub implements AddStockRepository {
-      async add({}): Promise<void> {
-        return
-      }
-    }
-    const addStockRepositoryStub = new AddStockRepositoryStub()
-    const sut = new AddStockUseCase(addStockRepositoryStub)
+    const { sut, addStockRepositoryStub } = makeSut()
     const addStockSpy = jest.spyOn(addStockRepositoryStub, 'add')
-    await sut.execute({
-      modelName: 'any_name',
-      color: 'any_color',
-      quantity: 1,
-    })
-    expect(addStockSpy).toHaveBeenCalledWith({
-      modelName: 'any_name',
-      color: 'any_color',
-      quantity: 1,
-    })
+    await sut.execute(makeStockDataRequest())
+    expect(addStockSpy).toHaveBeenCalledWith(makeStockDataRequest())
   })
 })
