a1d5a4d73d07a649929a441131c1d31e2f2ff767
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-16 00:46:22 -0300

    feat: ensure AddStockUseCase returns an error if stock name and color name already exists

--- src/data/usecases/stock/add-stock/addStockUseCase.ts
+++ src/data/usecases/stock/add-stock/addStockUseCase.ts
@@ -8,7 +8,12 @@ export class AddStockUseCase implements AddStock {
     private readonly loadStockByNameRepository: LoadStockByNameRepository
   ) {}
   async execute(stockData: AddStock.Params): Promise<AddStock.Result> {
-    await this.loadStockByNameRepository.loadByName(stockData.modelName)
+    const stock = await this.loadStockByNameRepository.loadByName(
+      stockData.modelName
+    )
+    if (stock && stock.color === stockData.color) {
+      throw new Error('A stock with this name already exists')
+    }
     await this.addStockRepository.add(stockData)
     return
   }
