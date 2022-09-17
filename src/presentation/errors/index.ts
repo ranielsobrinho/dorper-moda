b577a1684988cb78925a0b9624938d4c3b0bb9bc
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-17 14:48:55 -0300

    refactor: remove unused files

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,3 +1,1 @@
-export * from './invalid-param-error'
-export * from './missing-param-error'
 export * from './server-error'
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,16 +1,6 @@
 import { HttpResponse } from '../protocols/http'
 import { ServerError } from '../errors/'
 
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400,
-  body: error
-})
-
-export const forbidden = (error: Error): HttpResponse => ({
-  statusCode: 403,
-  body: error
-})
-
 export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.message)
