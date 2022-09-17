72846d64b0eacd04bd06622106cd68323a11c955
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-17 15:21:57 -0300

    feat: ensure AddStockController returns 500 if use case throws

--- src/presentation/controllers/stock/add-stock/add-stock-controller.ts
+++ src/presentation/controllers/stock/add-stock/add-stock-controller.ts
@@ -1,18 +1,31 @@
 import { AddStock } from '../../../../domain/usecases/stock/add-stock'
-import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
+import { serverError } from '../../../helpers/http-helper'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation
+} from '../../../protocols'
 
 export class AddStockController implements Controller {
-  constructor(private readonly addStockUseCase: AddStock) {}
+  constructor(
+    private readonly addStockUseCase: AddStock,
+    private readonly validation: Validation
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { modelName, color, quantity } = httpRequest.body
-    await this.addStockUseCase.execute({
-      modelName,
-      color,
-      quantity
-    })
-    return {
-      statusCode: 204,
-      body: 'ok'
+    try {
+      const { modelName, color, quantity } = httpRequest.body
+      await this.addStockUseCase.execute({
+        modelName,
+        color,
+        quantity
+      })
+      return {
+        statusCode: 204,
+        body: 'ok'
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
