ea1eba62b42f516f29267552cd2304cae6a90df4
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-17 14:48:13 -0300

    test: ensure AddStockController calls use case with correct values

---
+++ src/presentation/controllers/stock/add-stock/add-stock-controller.spec.ts
@@ -0,0 +1,40 @@
+import { AddStock } from '../../../../domain/usecases/stock/add-stock'
+import { HttpRequest } from '../../../protocols'
+import { AddStockController } from './add-stock-controller'
+
+const makeAddStockRequest = (): HttpRequest => ({
+  body: {
+    modelName: 'any_name',
+    color: 'any_color',
+    quantity: 1
+  }
+})
+
+const makeAddStockStub = (): AddStock => {
+  class AddStockStub implements AddStock {
+    async execute(params: AddStock.Params): Promise<void> {}
+  }
+  return new AddStockStub()
+}
+
+type SutTypes = {
+  sut: AddStockController
+  addStockStub: AddStock
+}
+
+const makeSut = (): SutTypes => {
+  const addStockStub = makeAddStockStub()
+  const sut = new AddStockController(addStockStub)
+  return {
+    sut,
+    addStockStub
+  }
+}
+describe('AddStockController', () => {
+  test('Should call AddStock with correct values', async () => {
+    const { sut, addStockStub } = makeSut()
+    const addSpy = jest.spyOn(addStockStub, 'execute')
+    await sut.handle(makeAddStockRequest())
+    expect(addSpy).toHaveBeenCalledWith(makeAddStockRequest().body)
+  })
+})
