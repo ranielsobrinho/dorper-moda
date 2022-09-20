28b0a7394f0995738093bede8e818a1463339393
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-20 02:15:49 -0300

    feat: add get stock by id use case

---
+++ src/domain/usecases/stock/get-stock-by-id.ts
@@ -0,0 +1,9 @@
+import { StockModel } from '../../models/stock'
+
+export interface GetStockById {
+  execute(stockId: string): Promise<GetStockById.Result>
+}
+
+export namespace GetStockById {
+  export type Result = StockModel
+}
