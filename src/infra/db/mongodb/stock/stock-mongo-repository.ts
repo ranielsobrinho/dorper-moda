8d34f29ca4c0744de0c7f2bc631574b175838459
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-20 00:15:39 -0300

    feat: ensure StockMongoRepository returns an array of stock on loadAll success

--- src/infra/db/mongodb/stock/stock-mongo-repository.ts
+++ src/infra/db/mongodb/stock/stock-mongo-repository.ts
@@ -1,9 +1,13 @@
 import { AddStockRepository } from '../../../../data/protocols/db/stock/addStockRepository'
 import { LoadStockByNameRepository } from '../../../../data/protocols/db/stock/loadStockByNameRepository'
+import { LoadStocksRepository } from '../../../../data/protocols/db/stock/loadStocksRepository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class StockMongoRepository
-  implements AddStockRepository, LoadStockByNameRepository
+  implements
+    AddStockRepository,
+    LoadStockByNameRepository,
+    LoadStocksRepository
 {
   async add(
     stockData: AddStockRepository.Params
@@ -16,5 +20,11 @@ export class StockMongoRepository
     const stockCollection = MongoHelper.getCollection('stocks')
     const stockData = await stockCollection.findOne({ modelName: name })
     return stockData && MongoHelper.map(stockData)
+  }
+
+  async loadAll(): Promise<LoadStocksRepository.Result> {
+    const stockCollection = MongoHelper.getCollection('stocks')
+    const stockData = await stockCollection.find().toArray()
+    return MongoHelper.mapCollection(stockData)
   }
 }
