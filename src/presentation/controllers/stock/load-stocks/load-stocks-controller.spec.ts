b74898011407c5ddcc22cf8590edda2fcf15cf30
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-20 00:39:59 -0300

    test: ensure LoadStocksController returns 500 if LoadStocks throws

--- src/presentation/controllers/stock/load-stocks/load-stocks-controller.spec.ts
+++ src/presentation/controllers/stock/load-stocks/load-stocks-controller.spec.ts
@@ -1,6 +1,7 @@
 import { StockModel } from '../../../../domain/models/stock'
 import { LoadStocks } from '../../../../domain/usecases/stock/load-stock'
 import { LoadStocksController } from './load-stocks-controller'
+import { serverError } from '../../../helpers/http-helper'
 
 const makeFakeStockModel = (): StockModel[] => {
   return [
@@ -42,5 +43,14 @@ describe('LoadStocksController', () => {
     const loadAllSpy = jest.spyOn(loadStocksStub, 'loadAll')
     await sut.handle({})
     expect(loadAllSpy).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should return 500 if LoadStocks throws', async () => {
+    const { sut, loadStocksStub } = makeSut()
+    jest.spyOn(loadStocksStub, 'loadAll').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
