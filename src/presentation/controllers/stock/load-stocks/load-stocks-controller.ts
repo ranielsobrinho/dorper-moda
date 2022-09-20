30cc5ebf0a2fba77013bd57f821c87494a3747a1
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-20 00:50:29 -0300

    feat: ensure LoadStockController returns 200 on LoadStocks success or 204 if LoadStocks returns empty

--- src/presentation/controllers/stock/load-stocks/load-stocks-controller.ts
+++ src/presentation/controllers/stock/load-stocks/load-stocks-controller.ts
@@ -1,5 +1,5 @@
 import { LoadStocks } from '../../../../domain/usecases/stock/load-stock'
-import { serverError } from '../../../helpers/http-helper'
+import { serverError, ok, noContent } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadStocksController implements Controller {
@@ -7,11 +7,8 @@ export class LoadStocksController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadStocksUseCase.loadAll()
-      return Promise.resolve({
-        statusCode: 200,
-        body: null
-      })
+      const stockData = await this.loadStocksUseCase.loadAll()
+      return stockData.length ? ok(stockData) : noContent()
     } catch (error) {
       return serverError(error)
     }
