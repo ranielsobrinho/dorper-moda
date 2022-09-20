5008b0cbc5ccd2d72a1c27f1c25b162c8a4ea84b
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-20 00:02:32 -0300

    feat: ensure LoadStockUseCase returns an array of stocks on success

--- src/data/usecases/stock/load-stocks/loadStocksUseCase.ts
+++ src/data/usecases/stock/load-stocks/loadStocksUseCase.ts
@@ -5,14 +5,7 @@ export class LoadStocksUseCase implements LoadStocks {
   constructor(private readonly loadStocksRepository: LoadStocksRepository) {}
 
   async loadAll(): Promise<LoadStocks.Result> {
-    await this.loadStocksRepository.loadAll()
-    return Promise.resolve([
-      {
-        id: 'any_id',
-        modelName: 'any_name',
-        color: 'any_color',
-        quantity: 1
-      }
-    ])
+    const stock = await this.loadStocksRepository.loadAll()
+    return stock
   }
 }
