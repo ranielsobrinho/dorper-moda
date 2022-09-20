c33767a9dc4f9465728304582075e53101b02680
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-20 00:39:37 -0300

    feat: ensure LoadStocksController returns 500 if LoadStocks throws

--- src/presentation/controllers/stock/load-stocks/load-stocks-controller.ts
+++ src/presentation/controllers/stock/load-stocks/load-stocks-controller.ts
@@ -1,14 +1,19 @@
 import { LoadStocks } from '../../../../domain/usecases/stock/load-stock'
+import { serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadStocksController implements Controller {
   constructor(private readonly loadStocksUseCase: LoadStocks) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadStocksUseCase.loadAll()
-    return Promise.resolve({
-      statusCode: 200,
-      body: null
-    })
+    try {
+      await this.loadStocksUseCase.loadAll()
+      return Promise.resolve({
+        statusCode: 200,
+        body: null
+      })
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
