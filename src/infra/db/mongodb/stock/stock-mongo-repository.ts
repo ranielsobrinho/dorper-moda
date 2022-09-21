72afc113d7c3e687aa96971e307c27fa334bc830
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-21 00:28:51 -0300

    feat: ensure StockMongoRepository returns a stock on getById method

--- src/infra/db/mongodb/stock/stock-mongo-repository.ts
+++ src/infra/db/mongodb/stock/stock-mongo-repository.ts
@@ -1,13 +1,16 @@
 import { AddStockRepository } from '../../../../data/protocols/db/stock/addStockRepository'
+import { GetStockByIdRepository } from '../../../../data/protocols/db/stock/getStockByIdRepository'
 import { LoadStockByNameRepository } from '../../../../data/protocols/db/stock/loadStockByNameRepository'
 import { LoadStocksRepository } from '../../../../data/protocols/db/stock/loadStocksRepository'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class StockMongoRepository
   implements
     AddStockRepository,
     LoadStockByNameRepository,
-    LoadStocksRepository
+    LoadStocksRepository,
+    GetStockByIdRepository
 {
   async add(
     stockData: AddStockRepository.Params
@@ -26,5 +29,12 @@ export class StockMongoRepository
     const stockCollection = MongoHelper.getCollection('stocks')
     const stockData = await stockCollection.find().toArray()
     return MongoHelper.mapCollection(stockData)
+  }
+
+  async getById(stockId: string): Promise<GetStockByIdRepository.Result> {
+    const stockCollection = MongoHelper.getCollection('stocks')
+    const objectId = new ObjectId(stockId)
+    const stockData = await stockCollection.findOne({ _id: objectId })
+    return stockData && MongoHelper.map(stockData)
   }
 }
