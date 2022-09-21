101a1177ce94a6a505cefdaee2592788b719f7bb
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-21 00:45:22 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/stock/delete-stock/deleteStockUseCase.spec.ts
+++ src/data/usecases/stock/delete-stock/deleteStockUseCase.spec.ts
@@ -1,13 +1,30 @@
 import { DeleteStockRepository } from '../../../protocols/db/stock/deleteStockRepository'
 import { DeleteStockUseCase } from './deleteStockUseCase'
 
+const makeDeleteStockRepositoryStub = (): DeleteStockRepository => {
+  class DeleteStockRepositoryStub implements DeleteStockRepository {
+    async delete(stockId: string): Promise<void> {}
+  }
+  return new DeleteStockRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DeleteStockUseCase
+  deleteStockRepositoryStub: DeleteStockRepository
+}
+
+const makeSut = (): SutTypes => {
+  const deleteStockRepositoryStub = makeDeleteStockRepositoryStub()
+  const sut = new DeleteStockUseCase(deleteStockRepositoryStub)
+  return {
+    sut,
+    deleteStockRepositoryStub
+  }
+}
+
 describe('DeleteStockUseCase', () => {
   test('Should call DeleteStockRepository with correct values', async () => {
-    class DeleteStockRepositoryStub implements DeleteStockRepository {
-      async delete(stockId: string): Promise<void> {}
-    }
-    const deleteStockRepositoryStub = new DeleteStockRepositoryStub()
-    const sut = new DeleteStockUseCase(deleteStockRepositoryStub)
+    const { sut, deleteStockRepositoryStub } = makeSut()
     const deleteSpy = jest.spyOn(deleteStockRepositoryStub, 'delete')
     await sut.execute('1')
     expect(deleteSpy).toHaveBeenCalledWith('1')
