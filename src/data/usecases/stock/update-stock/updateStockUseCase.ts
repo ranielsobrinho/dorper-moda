2a1ead042cc3834109c7bd9ff466250c709c4ef8
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-21 01:47:02 -0300

    feat: ensure UpdateStockUseCase calls GetStockByIdRepository with correct values

--- src/data/usecases/stock/update-stock/updateStockUseCase.ts
+++ src/data/usecases/stock/update-stock/updateStockUseCase.ts
@@ -1,10 +1,15 @@
 import { UpdateStock } from '../../../../domain/usecases/stock/update-stock'
+import { GetStockByIdRepository } from '../../../protocols/db/stock/getStockByIdRepository'
 import { UpdateStockRepository } from '../../../protocols/db/stock/updateStockRepository'
 
 export class UpdateStockUseCase implements UpdateStock {
-  constructor(private readonly updateStockRepository: UpdateStockRepository) {}
+  constructor(
+    private readonly updateStockRepository: UpdateStockRepository,
+    private readonly getStockByIdRepository: GetStockByIdRepository
+  ) {}
 
   async execute(params: UpdateStock.Params): Promise<UpdateStock.Result> {
+    await this.getStockByIdRepository.getById(params.stockId)
     await this.updateStockRepository.update(params)
   }
 }
