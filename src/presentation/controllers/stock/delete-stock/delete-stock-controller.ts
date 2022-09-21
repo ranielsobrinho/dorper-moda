55bd590ccd1e6d1f6c193304703c6c0a080a9572
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-21 18:35:04 -0300

    feat: ensure DeleteStockController returns 500 if DeleteStock throws

--- src/presentation/controllers/stock/delete-stock/delete-stock-controller.ts
+++ src/presentation/controllers/stock/delete-stock/delete-stock-controller.ts
@@ -1,12 +1,16 @@
 import { DeleteStock } from '../../../../domain/usecases/stock/delete-stock'
-import { noContent } from '../../../helpers/http-helper'
+import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class DeleteStockController implements Controller {
   constructor(private readonly deleteStock: DeleteStock) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { stockId } = httpRequest.body
-    await this.deleteStock.execute(stockId)
-    return noContent()
+    try {
+      const { stockId } = httpRequest.body
+      await this.deleteStock.execute(stockId)
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
