a8eccc17e2e87ea1f4533c5dca375d466b585302
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-21 00:11:06 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/stock/get-by-id/getByIdUseCase.spec.ts
+++ src/data/usecases/stock/get-by-id/getByIdUseCase.spec.ts
@@ -9,15 +9,32 @@ const makeFakeStockData = (): StockModel => ({
   quantity: 1
 })
 
+const makeGetStockByIdRepositoryStub = (): GetStockByIdRepository => {
+  class GetStockByIdRepositoryStub implements GetStockByIdRepository {
+    async getById(stockId: string): Promise<GetStockByIdRepository.Result> {
+      return Promise.resolve(makeFakeStockData())
+    }
+  }
+  return new GetStockByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: GetStockByIdUseCase
+  getStockByIdRepositoryStub: GetStockByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getStockByIdRepositoryStub = makeGetStockByIdRepositoryStub()
+  const sut = new GetStockByIdUseCase(getStockByIdRepositoryStub)
+  return {
+    sut,
+    getStockByIdRepositoryStub
+  }
+}
+
 describe('GetByIdUseCase', () => {
   test('Should call GetStockByIdRepository with correct values', async () => {
-    class GetStockByIdRepositoryStub implements GetStockByIdRepository {
-      async getById(stockId: string): Promise<GetStockByIdRepository.Result> {
-        return Promise.resolve(makeFakeStockData())
-      }
-    }
-    const getStockByIdRepositoryStub = new GetStockByIdRepositoryStub()
-    const sut = new GetStockByIdUseCase(getStockByIdRepositoryStub)
+    const { sut, getStockByIdRepositoryStub } = makeSut()
     const getByIdSpy = jest.spyOn(getStockByIdRepositoryStub, 'getById')
     await sut.execute('any_id')
     expect(getByIdSpy).toHaveBeenCalledWith('any_id')
