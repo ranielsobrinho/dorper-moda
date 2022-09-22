20977d27bebc39fc9cd2f03ca04156fd1b0c77c1
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-22 01:24:45 -0300

    refactor: remove unused code

--- src/data/usecases/stock/delete-stock/deleteStockUseCase.ts
+++ src/data/usecases/stock/delete-stock/deleteStockUseCase.ts
@@ -1,18 +1,10 @@
 import { DeleteStock } from '../../../../domain/usecases/stock/delete-stock'
 import { DeleteStockRepository } from '../../../protocols/db/stock/deleteStockRepository'
-import { GetStockByIdRepository } from '../../../protocols/db/stock/getStockByIdRepository'
 
 export class DeleteStockUseCase implements DeleteStock {
-  constructor(
-    private readonly deleteStockRepository: DeleteStockRepository,
-    private readonly getStockByIdRepository: GetStockByIdRepository
-  ) {}
+  constructor(private readonly deleteStockRepository: DeleteStockRepository) {}
 
   async execute(stockId: string): Promise<void | null> {
-    const stockData = await this.getStockByIdRepository.getById(stockId)
-    if (!stockData) {
-      return null
-    }
     await this.deleteStockRepository.delete(stockId)
   }
 }
