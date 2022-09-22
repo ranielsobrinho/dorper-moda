e4d2a7b7cac49b2bb814b1af2cff6319e00edbdb
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-22 00:59:49 -0300

    feat: ensure DeleteStockUseCase calls GetStockByIdRepository with correct values

--- src/data/usecases/stock/delete-stock/deleteStockUseCase.ts
+++ src/data/usecases/stock/delete-stock/deleteStockUseCase.ts
@@ -1,10 +1,15 @@
 import { DeleteStock } from '../../../../domain/usecases/stock/delete-stock'
 import { DeleteStockRepository } from '../../../protocols/db/stock/deleteStockRepository'
+import { GetStockByIdRepository } from '../../../protocols/db/stock/getStockByIdRepository'
 
 export class DeleteStockUseCase implements DeleteStock {
-  constructor(private readonly deleteStockRepository: DeleteStockRepository) {}
+  constructor(
+    private readonly deleteStockRepository: DeleteStockRepository,
+    private readonly getStockByIdRepository: GetStockByIdRepository
+  ) {}
 
   async execute(stockId: string): Promise<void> {
+    await this.getStockByIdRepository.getById(stockId)
     await this.deleteStockRepository.delete(stockId)
   }
 }
