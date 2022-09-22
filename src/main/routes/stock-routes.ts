dc0f4569af7ed09ba4b290466adf8486826cffbf
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-22 15:26:26 -0300

    feat: ensure update stock route returns 204 on success

--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -4,10 +4,12 @@ import { makeAddStockController } from '../factories/controllers/stock/add-stock
 import { makeDeleteStockController } from '../factories/controllers/stock/deleteStock/delete-stock-factory'
 import { makeGetByIdController } from '../factories/controllers/stock/get-by-id/get-by-id-factory'
 import { makeLoadStocksController } from '../factories/controllers/stock/load-stocks/load-stocks-factory'
+import { makeUpdateStockController } from '../factories/controllers/stock/update-stock/update-stock-factory'
 
 export default (router: Router): void => {
   router.post('/stock', adaptRoute(makeAddStockController()))
   router.get('/stock', adaptRoute(makeLoadStocksController()))
   router.get('/stock/:stockId', adaptRoute(makeGetByIdController()))
   router.delete('/stock/:stockId', adaptRoute(makeDeleteStockController()))
+  router.put('/stock/:stockId', adaptRoute(makeUpdateStockController()))
 }
