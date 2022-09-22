e00f0b4330dd5cdbca66657571800d2f12eef546
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-22 13:51:50 -0300

    feat: ensure UpdateStockController returns 403 if UpdateStock returns null

--- src/presentation/controllers/stock/update-stock/update-stock-controller.ts
+++ src/presentation/controllers/stock/update-stock/update-stock-controller.ts
@@ -1,5 +1,6 @@
 import { UpdateStock } from '../../../../domain/usecases/stock/update-stock'
-import { noContent, serverError } from '../../../helpers/http-helper'
+import { InvalidParamError } from '../../../errors'
+import { forbidden, noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class UpdateStockController implements Controller {
@@ -7,7 +8,10 @@ export class UpdateStockController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.updateStock.execute(httpRequest.body)
+      const stockData = await this.updateStock.execute(httpRequest.body)
+      if (!stockData) {
+        return forbidden(new InvalidParamError('stockId'))
+      }
       return Promise.resolve(noContent())
     } catch (error) {
       return serverError(error)
