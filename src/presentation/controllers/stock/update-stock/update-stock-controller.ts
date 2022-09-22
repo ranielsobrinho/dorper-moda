aab0a17e6b2c19ea72a62156ae497ecf39d5d3df
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-22 17:41:43 -0300

    feat: ensure UpdateStockController calls Validation with correct values

--- src/presentation/controllers/stock/update-stock/update-stock-controller.ts
+++ src/presentation/controllers/stock/update-stock/update-stock-controller.ts
@@ -1,15 +1,24 @@
 import { UpdateStock } from '../../../../domain/usecases/stock/update-stock'
 import { InvalidParamError } from '../../../errors'
 import { forbidden, noContent, serverError } from '../../../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation
+} from '../../../protocols'
 
 export class UpdateStockController implements Controller {
-  constructor(private readonly updateStock: UpdateStock) {}
+  constructor(
+    private readonly updateStock: UpdateStock,
+    private readonly validation: Validation
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { stockId } = httpRequest.params
       const { data } = httpRequest.body
+      this.validation.validate(data)
       const stockData = await this.updateStock.execute({
         stockId,
         data
