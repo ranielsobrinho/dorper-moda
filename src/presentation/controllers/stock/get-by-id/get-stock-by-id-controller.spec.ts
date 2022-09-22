ee632182b0fe5fafca72bd864b47a7355ebbcadb
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-22 00:35:28 -0300

    test: ensure GetStockByIdController returns 500  if GetStockById throws

--- src/presentation/controllers/stock/get-by-id/get-stock-by-id-controller.spec.ts
+++ src/presentation/controllers/stock/get-by-id/get-stock-by-id-controller.spec.ts
@@ -2,6 +2,7 @@ import { GetStockById } from '../../../../domain/usecases/stock/get-stock-by-id'
 import { StockModel } from '../../../../domain/models/stock'
 import { HttpRequest } from '../../../protocols/http'
 import { GetStockByIdController } from './get-stock-by-id-controller'
+import { serverError } from '../../../helpers/http-helper'
 
 const makeFakeStockModel = (): StockModel => ({
   id: 'any_id',
@@ -40,10 +41,17 @@ const makeSut = (): SutTypes => {
 }
 
 describe('GetStockByIdController', () => {
-  test('Should call GetById with correct values', async () => {
+  test('Should call GetStockById with correct values', async () => {
     const { sut, getByIdStub } = makeSut()
     const getByIdSpy = jest.spyOn(getByIdStub, 'execute')
     await sut.handle(httpRequest())
     expect(getByIdSpy).toHaveBeenCalledWith(httpRequest().body.stockId)
+  })
+
+  test('Should return 500 if GetStockById throws', async () => {
+    const { sut, getByIdStub } = makeSut()
+    jest.spyOn(getByIdStub, 'execute').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(httpRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
