de6a7c760e4910156fc884b5c140e75a36e992bc
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-22 00:35:03 -0300

    feat: ensure GetStockByIdController returns 500 if GetStockById throws

--- src/presentation/controllers/stock/get-by-id/get-stock-by-id-controller.ts
+++ src/presentation/controllers/stock/get-by-id/get-stock-by-id-controller.ts
@@ -1,13 +1,16 @@
 import { GetStockById } from '../../../../domain/usecases/stock/get-stock-by-id'
-import { noContent } from '../../../helpers/http-helper'
+import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class GetStockByIdController implements Controller {
   constructor(private readonly getStockById: GetStockById) {}
-
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { stockId } = httpRequest.body
-    await this.getStockById.execute(stockId)
-    return Promise.resolve(noContent())
+    try {
+      const { stockId } = httpRequest.body
+      await this.getStockById.execute(stockId)
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
