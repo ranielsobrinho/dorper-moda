1011deb551b34838b0a649f3a73a3da241e7f6bc
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-22 15:21:05 -0300

    feat: ensure get-by-id and delete stock route is called correctly

--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddStockController } from '../factories/controllers/stock/add-stock/add-stock-factory'
+import { makeDeleteStockController } from '../factories/controllers/stock/deleteStock/delete-stock-factory'
+import { makeGetByIdController } from '../factories/controllers/stock/get-by-id/get-by-id-factory'
 import { makeLoadStocksController } from '../factories/controllers/stock/load-stocks/load-stocks-factory'
 
 export default (router: Router): void => {
   router.post('/stock', adaptRoute(makeAddStockController()))
   router.get('/stock', adaptRoute(makeLoadStocksController()))
+  router.get('/stock/:stockId', adaptRoute(makeGetByIdController()))
+  router.delete('/stock/:stockId', adaptRoute(makeDeleteStockController()))
 }
