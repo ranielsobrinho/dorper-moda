cae77aabe7d4cec856373915238f84effbbd9667
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-23 01:23:45 -0300

    feat: add docker file and docker compose file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl:
-    process.env.MONGO_URL ?? 'mongodb://localhost:27017/dorper-moda-api',
-  port: process.env.PORT ?? 5000
+    process.env.MONGO_URL || 'mongodb://localhost:27017/dorper-moda-api',
+  port: process.env.PORT || 5000
 }
