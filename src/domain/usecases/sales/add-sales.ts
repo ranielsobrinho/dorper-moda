f601d5cfeb17556a9dc4e7485a288a4eb85f4815
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-23 18:55:19 -0300

    feat: add use case to create a new sales model

---
+++ src/domain/usecases/sales/add-sales.ts
@@ -0,0 +1,10 @@
+import { SalesModel } from '../../models/sales'
+
+export interface CreateSale {
+  execute(params: CreateSale.Params): Promise<CreateSale.Result>
+}
+
+export namespace CreateSale {
+  export type Params = Omit<SalesModel, 'id'>
+  export type Result = void
+}
