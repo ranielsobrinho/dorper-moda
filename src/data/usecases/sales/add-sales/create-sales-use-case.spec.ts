a5e6ed760d32e232bbeb7b931f743330b1f12c9e
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-24 00:44:46 -0300

    test: ensure CreateSalesUseCase calls LoadStockByNameRepository with correct values

---
+++ src/data/usecases/sales/add-sales/create-sales-use-case.spec.ts
@@ -0,0 +1,59 @@
+import { StockModel } from '../../../../domain/models/stock'
+import { SalesModel } from '../../../../domain/models/sales'
+import { LoadStockByNameRepository } from '../../../protocols/db/stock/loadStockByNameRepository'
+import { CreateSalesUseCase } from './create-sales-use-case'
+
+const makeStockData = (): StockModel => ({
+  id: 'any_id',
+  modelName: 'any_name',
+  color: 'any_color',
+  quantity: 1
+})
+
+const makeSalesRequest = (): SalesModel => ({
+  id: 'any_id',
+  clientName: 'any_client_name',
+  deliveryFee: 15.0,
+  paymentForm: 'MASTER CARD CREDIT',
+  products: [
+    {
+      modelName: 'any_model_name',
+      color: 'any_color',
+      quantity: 2
+    }
+  ],
+  soldAt: new Date(),
+  total: 100.0
+})
+
+const makeLoadStockByNameRepositoryStub = (): LoadStockByNameRepository => {
+  class LoadStockByNameRepositoryStub implements LoadStockByNameRepository {
+    async loadByName(name: string): Promise<LoadStockByNameRepository.Result> {
+      return Promise.resolve(makeStockData())
+    }
+  }
+  return new LoadStockByNameRepositoryStub()
+}
+
+type SutTypes = {
+  sut: CreateSalesUseCase
+  loadStockByNameRepositoryStub: LoadStockByNameRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadStockByNameRepositoryStub = makeLoadStockByNameRepositoryStub()
+  const sut = new CreateSalesUseCase(loadStockByNameRepositoryStub)
+  return {
+    sut,
+    loadStockByNameRepositoryStub
+  }
+}
+
+describe('AddSalesUseCase', () => {
+  test('Should call GetStockByNameRepository with correct value', async () => {
+    const { sut, loadStockByNameRepositoryStub } = makeSut()
+    const getStockSpy = jest.spyOn(loadStockByNameRepositoryStub, 'loadByName')
+    await sut.execute(makeSalesRequest())
+    expect(getStockSpy).toHaveBeenCalledWith('any_model_name')
+  })
+})
