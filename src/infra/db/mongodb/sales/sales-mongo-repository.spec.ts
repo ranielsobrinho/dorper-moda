bfb1fb319219aa3acc78f772b02f7bcd1a39898a
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-25 00:48:39 -0300

    test: ensure SalesMongoRepository creates a sale on success

---
+++ src/infra/db/mongodb/sales/sales-mongo-repository.spec.ts
@@ -0,0 +1,52 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { SalesMongoRepository } from './sales-mongo-repository'
+import MockDate from 'mockdate'
+
+const makeFakeSaleRequest = () => ({
+  clientName: 'any_client_name',
+  deliveryFee: 25,
+  paymentForm: 'CREDIT CARD',
+  products: [
+    {
+      modelName: 'any_model_name',
+      color: 'any_color_name',
+      quantity: 1
+    }
+  ],
+  soldAt: new Date(),
+  total: 110
+})
+
+const makeSut = (): SalesMongoRepository => {
+  return new SalesMongoRepository()
+}
+
+let salesCollection: Collection
+describe('SalesMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+    MockDate.set(new Date())
+  })
+
+  beforeEach(async () => {
+    salesCollection = MongoHelper.getCollection('sales')
+    await salesCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    MockDate.reset()
+  })
+
+  test('Should create a new sales data on success', async () => {
+    const sut = makeSut()
+    await sut.create(makeFakeSaleRequest())
+    const salesData = await salesCollection.findOne({
+      clientName: 'any_client_name'
+    })
+    expect(salesData).toBeTruthy()
+    expect(salesData?._id).toBeTruthy()
+    expect(salesData?.clientName).toEqual('any_client_name')
+  })
+})
