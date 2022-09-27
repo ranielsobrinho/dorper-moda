2db7be23797db2b56128560c18a35044aae6dc3d
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-27 14:04:09 -0300

    feat: ensure CreateSalesController returns 400 if Validations returns a error

--- src/presentation/controllers/sales/create-sales/create-sales-controller.ts
+++ src/presentation/controllers/sales/create-sales/create-sales-controller.ts
@@ -1,12 +1,28 @@
 import { CreateSale } from '../../../../domain/usecases/sales/create-sales'
-import { noContent, serverError } from '../../../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
+import {
+  badRequest,
+  noContent,
+  serverError
+} from '../../../helpers/http-helper'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation
+} from '../../../protocols'
 
 export class CreateSalesController implements Controller {
-  constructor(private readonly createSales: CreateSale) {}
+  constructor(
+    private readonly createSales: CreateSale,
+    private readonly validation: Validation
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       await this.createSales.execute(httpRequest.body)
       return noContent()
     } catch (error) {
