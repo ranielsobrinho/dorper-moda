837c220f8fb826a2c22b898dc6110d139fa0de4e
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-28 11:50:40 -0300

    feat: ensure GetSalesController retusn 200 on success

--- src/presentation/controllers/sales/get-sales/get-sales-controller.ts
+++ src/presentation/controllers/sales/get-sales/get-sales-controller.ts
@@ -1,5 +1,5 @@
 import { GetSales } from '../../../../domain/usecases/sales/get-sales'
-import { noContent, serverError } from '../../../helpers/http-helper'
+import { noContent, serverError, ok } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class GetSalesController implements Controller {
@@ -7,8 +7,8 @@ export class GetSalesController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.getSalesUseCase.getAll()
-      return Promise.resolve(noContent())
+      const salesData = await this.getSalesUseCase.getAll()
+      return salesData.length ? ok(salesData) : noContent()
     } catch (error) {
       return serverError(error)
     }
