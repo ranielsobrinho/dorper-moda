0f991f81bea9f3f036b602d1251c48bd8c52adfe
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-28 11:21:18 -0300

    feat: ensure SalesMongoRepository return an array of sales on getAll success

--- src/infra/db/mongodb/sales/sales-mongo-repository.ts
+++ src/infra/db/mongodb/sales/sales-mongo-repository.ts
@@ -1,11 +1,20 @@
 import { CreateSalesRepository } from '../../../../data/protocols/db/sales/createSalesRepository'
+import { GetSalesRepository } from '../../../../data/protocols/db/sales/get-sales-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SalesMongoRepository implements CreateSalesRepository {
+export class SalesMongoRepository
+  implements CreateSalesRepository, GetSalesRepository
+{
   async create(
     params: CreateSalesRepository.Params
   ): Promise<CreateSalesRepository.Result> {
     const salesCollection = MongoHelper.getCollection('sales')
     await salesCollection.insertOne(params)
+  }
+
+  async getAll(): Promise<GetSalesRepository.Result> {
+    const salesCollection = MongoHelper.getCollection('sales')
+    const salesData = await salesCollection.find().toArray()
+    return MongoHelper.mapCollection(salesData)
   }
 }
