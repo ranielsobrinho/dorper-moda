329326b4cc5404dfb944c071d2d6d0e9bcaf8e80
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-28 11:10:49 -0300

    feat: ensure GetSalesUseCase returns sales on success

--- src/data/usecases/sales/get-sales/get-sales-use-case.ts
+++ src/data/usecases/sales/get-sales/get-sales-use-case.ts
@@ -6,24 +6,8 @@ export class GetSalesUseCase implements GetSales {
   constructor(private readonly getSalesRepository: GetSalesRepository) {}
 
   async getAll(): Promise<SalesModel[]> {
-    await this.getSalesRepository.getAll()
+    const sales = await this.getSalesRepository.getAll()
 
-    return [
-      {
-        id: 'any_id',
-        clientName: 'any_client_name',
-        deliveryFee: 25,
-        paymentForm: 'CREDIT CARD',
-        products: [
-          {
-            modelName: 'any_model_name',
-            color: 'any_color_name',
-            quantity: 1
-          }
-        ],
-        soldAt: new Date(),
-        total: 110
-      }
-    ]
+    return sales
   }
 }
