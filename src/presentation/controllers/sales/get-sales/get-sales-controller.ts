79e2cd13b46edce5973faf2435099fba1b4cf515
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-09-28 11:44:02 -0300

    feat: ensure GetSalesController returns 500 if GetSales throws

--- src/presentation/controllers/sales/get-sales/get-sales-controller.ts
+++ src/presentation/controllers/sales/get-sales/get-sales-controller.ts
@@ -1,12 +1,16 @@
 import { GetSales } from '../../../../domain/usecases/sales/get-sales'
-import { noContent } from '../../../helpers/http-helper'
+import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class GetSalesController implements Controller {
   constructor(private readonly getSalesUseCase: GetSales) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.getSalesUseCase.getAll()
-    return Promise.resolve(noContent())
+    try {
+      await this.getSalesUseCase.getAll()
+      return Promise.resolve(noContent())
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
