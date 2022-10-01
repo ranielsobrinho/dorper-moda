f84bfaf6c8711a1d2e25a07bac1f1f0a9271412a
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-01 15:26:04 -0300

    chore: add and config morgan

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,18 @@
 import express from 'express'
 import setupRoutes from './routes'
 import setupMiddlewares from './middlewares'
+import morgan from 'morgan'
+
+morgan.token('body', (req: express.Request) => {
+  const isNotGet = req.method !== 'GET'
+  if (isNotGet) {
+    return JSON.stringify(req.body)
+  }
+  return 'body-empty'
+})
 
 const app = express()
+app.use(morgan(':date[iso] :method :url :status :body - :total-time ms'))
 setupMiddlewares(app)
 setupRoutes(app)
 
