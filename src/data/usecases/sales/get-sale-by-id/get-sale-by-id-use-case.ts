7479a0b8f9c1f57dbd2773cab648fb785b0ed895
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-05 13:29:36 -0300

    feat: ensure GetSaleByIdUseCase returns a sale data on success

--- src/data/usecases/sales/get-sale-by-id/get-sale-by-id-use-case.ts
+++ src/data/usecases/sales/get-sale-by-id/get-sale-by-id-use-case.ts
@@ -7,25 +7,11 @@ export class GetSaleByIdUseCase implements GetSaleById {
 
   async getById(saleId: string): Promise<SalesModel | null> {
     const saleData = await this.getSaleByIdRepository.getById(saleId)
+
     if (!saleData) {
       return null
     }
-    const makeGetSale = (): GetSaleByIdRepository.Result => ({
-      id: 'any_id',
-      clientName: 'any_client_name',
-      deliveryFee: 25,
-      paymentForm: 'CREDIT CARD',
-      products: [
-        {
-          modelName: 'any_model_name',
-          color: 'any_color_name',
-          quantity: 1
-        }
-      ],
-      soldAt: new Date(),
-      total: 110
-    })
 
-    return Promise.resolve(makeGetSale())
+    return saleData
   }
 }
