5305c466bc4b5c19095be6f837ed1a6fad2e0eae
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-05 14:19:27 -0300

    feat: ensure GetSaleByIdController returns 500 if GetSaleById throws

--- src/presentation/controllers/sales/get-sale-by-id/get-sale-by-id-controller.ts
+++ src/presentation/controllers/sales/get-sale-by-id/get-sale-by-id-controller.ts
@@ -1,13 +1,17 @@
 import { GetSaleById } from '../../../../domain/usecases/sales/get-sale-by-id'
-import { noContent } from '../../../helpers/http-helper'
+import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class GetSaleByIdController implements Controller {
   constructor(private readonly getSaleById: GetSaleById) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { saleId } = httpRequest.body
-    await this.getSaleById.getById(saleId)
-    return Promise.resolve(noContent())
+    try {
+      const { saleId } = httpRequest.body
+      await this.getSaleById.getById(saleId)
+      return Promise.resolve(noContent())
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
