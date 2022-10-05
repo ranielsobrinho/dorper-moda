818993e74e639fb43d57f28094157ee42be19c41
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-05 14:23:32 -0300

    feat: ensure GetSaleByIdController returns 400 if GetSaleById returns null

--- src/presentation/controllers/sales/get-sale-by-id/get-sale-by-id-controller.ts
+++ src/presentation/controllers/sales/get-sale-by-id/get-sale-by-id-controller.ts
@@ -1,5 +1,10 @@
 import { GetSaleById } from '../../../../domain/usecases/sales/get-sale-by-id'
-import { noContent, serverError } from '../../../helpers/http-helper'
+import { InvalidParamError } from '../../../errors'
+import {
+  badRequest,
+  noContent,
+  serverError
+} from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class GetSaleByIdController implements Controller {
@@ -8,7 +13,10 @@ export class GetSaleByIdController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { saleId } = httpRequest.body
-      await this.getSaleById.getById(saleId)
+      const saleData = await this.getSaleById.getById(saleId)
+      if (!saleData) {
+        return badRequest(new InvalidParamError('saleId'))
+      }
       return Promise.resolve(noContent())
     } catch (error) {
       return serverError(error)
