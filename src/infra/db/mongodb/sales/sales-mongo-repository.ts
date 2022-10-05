af2dc2d7416d0636068e4c0a6bae308c54528d80
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-05 13:50:38 -0300

    feat: ensure getById returns a sale data on success

--- src/infra/db/mongodb/sales/sales-mongo-repository.ts
+++ src/infra/db/mongodb/sales/sales-mongo-repository.ts
@@ -1,9 +1,11 @@
 import { CreateSalesRepository } from '../../../../data/protocols/db/sales/createSalesRepository'
 import { GetSalesRepository } from '../../../../data/protocols/db/sales/get-sales-repository'
+import { GetSaleByIdRepository } from '../../../../data/protocols/db/sales/get-sale-by-id-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class SalesMongoRepository
-  implements CreateSalesRepository, GetSalesRepository
+  implements CreateSalesRepository, GetSalesRepository, GetSaleByIdRepository
 {
   async create(
     params: CreateSalesRepository.Params
@@ -16,5 +18,12 @@ export class SalesMongoRepository
     const salesCollection = MongoHelper.getCollection('sales')
     const salesData = await salesCollection.find().toArray()
     return MongoHelper.mapCollection(salesData)
+  }
+
+  async getById(saleId: string): Promise<GetSaleByIdRepository.Result> {
+    const salesCollection = MongoHelper.getCollection('sales')
+    const objectId = new ObjectId(saleId)
+    const saleData = await salesCollection.findOne({ _id: objectId })
+    return saleData && MongoHelper.map(saleData)
   }
 }
