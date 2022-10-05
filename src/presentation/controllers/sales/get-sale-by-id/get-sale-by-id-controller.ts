33b9abc3f82af2a73cb1fc8564dafc4438401822
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-05 14:27:33 -0300

    feat: ensure GetSaleByIdController returns 200 on success

--- src/presentation/controllers/sales/get-sale-by-id/get-sale-by-id-controller.ts
+++ src/presentation/controllers/sales/get-sale-by-id/get-sale-by-id-controller.ts
@@ -1,10 +1,6 @@
 import { GetSaleById } from '../../../../domain/usecases/sales/get-sale-by-id'
 import { InvalidParamError } from '../../../errors'
-import {
-  badRequest,
-  noContent,
-  serverError
-} from '../../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class GetSaleByIdController implements Controller {
@@ -14,10 +10,12 @@ export class GetSaleByIdController implements Controller {
     try {
       const { saleId } = httpRequest.body
       const saleData = await this.getSaleById.getById(saleId)
+
       if (!saleData) {
         return badRequest(new InvalidParamError('saleId'))
       }
-      return Promise.resolve(noContent())
+
+      return ok(saleData)
     } catch (error) {
       return serverError(error)
     }
