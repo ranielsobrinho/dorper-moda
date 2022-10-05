33c67019f0549fb846df3232ac1a7973c426cd44
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-05 14:24:31 -0300

    test: ensure GetSaleByIdController returns 400 if GetSaleById returns null

--- src/presentation/controllers/sales/get-sale-by-id/get-sale-by-id-controller.spec.ts
+++ src/presentation/controllers/sales/get-sale-by-id/get-sale-by-id-controller.spec.ts
@@ -1,6 +1,7 @@
 import { SalesModel } from '../../../../domain/models/sales'
 import { GetSaleById } from '../../../../domain/usecases/sales/get-sale-by-id'
-import { serverError } from '../../../helpers/http-helper'
+import { InvalidParamError } from '../../../errors'
+import { badRequest, serverError } from '../../../helpers/http-helper'
 import { HttpRequest } from '../../../protocols'
 import { GetSaleByIdController } from './get-sale-by-id-controller'
 
@@ -62,5 +63,12 @@ describe('GetSaleByIdController', () => {
     jest.spyOn(getSaleByIdStub, 'getById').mockRejectedValueOnce(new Error())
     const httpResponse = await sut.handle(httpRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 400 if GetSaleById returns null', async () => {
+    const { sut, getSaleByIdStub } = makeSut()
+    jest.spyOn(getSaleByIdStub, 'getById').mockResolvedValueOnce(null)
+    const httpResponse = await sut.handle(httpRequest())
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('saleId')))
   })
 })
