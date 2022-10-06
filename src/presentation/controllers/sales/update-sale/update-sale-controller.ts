a32f1570fb4517f6d0142ae8b010acf92edf0c3b
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-06 11:14:31 -0300

    feat: ensure UpdateSaleController returns 500 if UpdateSale throws

--- src/presentation/controllers/sales/update-sale/update-sale-controller.ts
+++ src/presentation/controllers/sales/update-sale/update-sale-controller.ts
@@ -1,17 +1,21 @@
 import { UpdateSale } from '../../../../domain/usecases/sales/update-sale'
-import { noContent } from '../../../helpers/http-helper'
+import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class UpdateSaleController implements Controller {
   constructor(private readonly updateSale: UpdateSale) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { saleId } = httpRequest.params
-    const { data } = httpRequest.body
-    await this.updateSale.execute({
-      saleId,
-      data
-    })
-    return Promise.resolve(noContent())
+    try {
+      const { saleId } = httpRequest.params
+      const { data } = httpRequest.body
+      await this.updateSale.execute({
+        saleId,
+        data
+      })
+      return Promise.resolve(noContent())
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
