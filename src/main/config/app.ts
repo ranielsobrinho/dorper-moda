014a94417042d2e9694c54d5654ba340353ba79d
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-15 00:13:05 -0300

    feat: add cors to project

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import setupRoutes from './routes'
 import setupMiddlewares from './middlewares'
 import morgan from 'morgan'
+import cors from 'cors'
 
 morgan.token('body', (req: express.Request) => {
   const isNotGet = req.method !== 'GET'
@@ -13,6 +14,15 @@ morgan.token('body', (req: express.Request) => {
 
 const app = express()
 app.use(morgan(':date[iso] :method :url :status :body - :total-time ms'))
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://dorper-moda-frontend.vercel.app/'
+    ],
+    methods: ['POST', 'GET', 'PUT', 'DELETE']
+  })
+)
 setupMiddlewares(app)
 setupRoutes(app)
 
