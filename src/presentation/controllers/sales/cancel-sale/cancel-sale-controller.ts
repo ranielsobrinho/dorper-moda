137052a3019620f53eb46c5228b8a8cffe4e5407
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-24 18:23:37 -0300

    feat: ensure CancelSaleController returns 400 if GetSaleById returns null

--- src/presentation/controllers/sales/cancel-sale/cancel-sale-controller.ts
+++ src/presentation/controllers/sales/cancel-sale/cancel-sale-controller.ts
@@ -1,6 +1,11 @@
 import { CancelSale } from '../../../../domain/usecases/sales/cancel-sale'
 import { GetSaleById } from '../../../../domain/usecases/sales/get-sale-by-id'
-import { noContent, serverError } from '../../../helpers/http-helper'
+import { InvalidParamError } from '../../../errors'
+import {
+  badRequest,
+  noContent,
+  serverError
+} from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class CancelSaleController implements Controller {
@@ -12,7 +17,10 @@ export class CancelSaleController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { saleId } = httpRequest.params
-      await this.getSaleById.getById(saleId)
+      const saleData = await this.getSaleById.getById(saleId)
+      if (!saleData) {
+        return badRequest(new InvalidParamError('saleId'))
+      }
       await this.cancelSale.cancel(saleId)
       return noContent()
     } catch (error) {
