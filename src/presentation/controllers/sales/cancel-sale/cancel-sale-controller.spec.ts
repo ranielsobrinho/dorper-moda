7336fa4b42d21d661fa0a868c9271b5b2d95d787
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-24 18:18:53 -0300

    test: ensure CancelSaleController calls GetSaleById with correct values

--- src/presentation/controllers/sales/cancel-sale/cancel-sale-controller.spec.ts
+++ src/presentation/controllers/sales/cancel-sale/cancel-sale-controller.spec.ts
@@ -1,12 +1,35 @@
+import { SalesModel } from '../../../../domain/models/sales'
 import { CancelSale } from '../../../../domain/usecases/sales/cancel-sale'
+import { GetSaleById } from '../../../../domain/usecases/sales/get-sale-by-id'
 import { noContent, serverError } from '../../../helpers/http-helper'
 import { HttpRequest } from '../../../protocols/http'
 import { CancelSaleController } from './cancel-sale-controller'
+import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
     saleId: 'any_id'
   }
+})
+
+const makeGetSale = (): SalesModel => ({
+  id: 'any_id',
+  clientName: 'any_client_name',
+  deliveryFee: 25,
+  paymentForm: 'CREDIT CARD',
+  products: [
+    {
+      modelName: 'any_model_name',
+      description: [
+        {
+          color: 'any_color_name',
+          quantity: 1
+        }
+      ]
+    }
+  ],
+  soldAt: new Date(),
+  total: 110
 })
 
 const makeCancelSaleStub = (): CancelSale => {
@@ -16,21 +39,47 @@ const makeCancelSaleStub = (): CancelSale => {
   return new CancelSaleStub()
 }
 
+const makeGetSaleByIdStub = (): GetSaleById => {
+  class GetSaleByIdStub implements GetSaleById {
+    async getById(saleId: string): Promise<SalesModel | null> {
+      return Promise.resolve(makeGetSale())
+    }
+  }
+  return new GetSaleByIdStub()
+}
+
 type SutTypes = {
   sut: CancelSaleController
+  getSaleByIdStub: GetSaleById
   cancelSaleStub: CancelSale
 }
 
 const makeSut = (): SutTypes => {
   const cancelSaleStub = makeCancelSaleStub()
-  const sut = new CancelSaleController(cancelSaleStub)
+  const getSaleByIdStub = makeGetSaleByIdStub()
+  const sut = new CancelSaleController(cancelSaleStub, getSaleByIdStub)
   return {
     sut,
-    cancelSaleStub
+    cancelSaleStub,
+    getSaleByIdStub
   }
 }
 
 describe('CancelSaleController', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+  test('Should call GetSaleById with correct values', async () => {
+    const { sut, getSaleByIdStub } = makeSut()
+    const getByIdSpy = jest.spyOn(getSaleByIdStub, 'getById')
+    await sut.handle(makeFakeRequest())
+    expect(getByIdSpy).toHaveBeenCalledWith(makeFakeRequest().params.saleId)
+  })
+
   test('Should call CancelSale with correct values', async () => {
     const { sut, cancelSaleStub } = makeSut()
     const cancelSpy = jest.spyOn(cancelSaleStub, 'cancel')
