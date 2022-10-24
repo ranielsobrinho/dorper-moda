25a84e6263ce2042c7c15f532a38d8ed1554cc75
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-10-24 18:17:31 -0300

    feat: ensure CancelSaleController calls GetSaleById with correct values

--- src/main/factories/controllers/sales/cancel-sale/cancel-sale-factory.ts
+++ src/main/factories/controllers/sales/cancel-sale/cancel-sale-factory.ts
@@ -1,6 +1,10 @@
 import { CancelSaleController } from '../../../../../presentation/controllers/sales/cancel-sale/cancel-sale-controller'
 import { makeCancelSaleUseCase } from '../../../usecases/sales/cancel-sale/cancel-sale-use-case'
+import { makeGetSaleByIdUseCase } from '../../../usecases/sales/get-sale-by-id/get-sale-by-id-use-case'
 
 export const makeCancelSaleController = (): CancelSaleController => {
-  return new CancelSaleController(makeCancelSaleUseCase())
+  return new CancelSaleController(
+    makeCancelSaleUseCase(),
+    makeGetSaleByIdUseCase()
+  )
 }
--- src/presentation/controllers/sales/cancel-sale/cancel-sale-controller.ts
+++ src/presentation/controllers/sales/cancel-sale/cancel-sale-controller.ts
@@ -1,13 +1,18 @@
 import { CancelSale } from '../../../../domain/usecases/sales/cancel-sale'
+import { GetSaleById } from '../../../../domain/usecases/sales/get-sale-by-id'
 import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class CancelSaleController implements Controller {
-  constructor(private readonly cancelSale: CancelSale) {}
+  constructor(
+    private readonly cancelSale: CancelSale,
+    private readonly getSaleById: GetSaleById
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { saleId } = httpRequest.params
+      await this.getSaleById.getById(saleId)
       await this.cancelSale.cancel(saleId)
       return noContent()
     } catch (error) {
