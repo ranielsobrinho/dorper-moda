5312975e680f068654b120d8d7cc25563560de02
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-05 13:00:51 -0300

    feat: ensure LoadStockByNameController returns 500 if LoadStockByName throws

--- src/presentation/controllers/stock/load-stock-by-name/load-stock-by-name-controller.ts
+++ src/presentation/controllers/stock/load-stock-by-name/load-stock-by-name-controller.ts
@@ -1,13 +1,17 @@
 import { LoadStockByName } from '../../../../domain/usecases/stock/load-stock-by-name'
-import { noContent } from '../../../helpers/http-helper'
+import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadStockByNameController implements Controller {
   constructor(private readonly loadStockByName: LoadStockByName) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { stockName } = httpRequest.body
-    await this.loadStockByName.loadByName(stockName)
-    return noContent()
+    try {
+      const { stockName } = httpRequest.body
+      await this.loadStockByName.loadByName(stockName)
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
