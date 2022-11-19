2bfcdddb6a1dde6c35aee804412b6c04bdf83584
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-19 17:25:03 -0300

    feat: ensure CreateAccountUseCase calls Encrypter with correct values

--- src/data/usecases/account/create-account/create-account-use-case.ts
+++ src/data/usecases/account/create-account/create-account-use-case.ts
@@ -1,19 +1,22 @@
 import { CreateAccount } from '../../../../domain/usecases/account/create-account'
+import { Encrypter } from '../../../protocols/criptography/encrypter'
 import { LoadAccountByUsernameRepository } from '../../../protocols/db/account/load-account-by-username-repository'
 
 export class CreateAccountUseCase implements CreateAccount {
   constructor(
-    private readonly loadAccountByUsernameRepository: LoadAccountByUsernameRepository
+    private readonly loadAccountByUsernameRepository: LoadAccountByUsernameRepository,
+    private readonly encrypter: Encrypter
   ) {}
 
   async execute(params: CreateAccount.Params): Promise<CreateAccount.Result> {
-    const { username } = params
+    const { username, password } = params
     const account = await this.loadAccountByUsernameRepository.loadByUsername(
       username
     )
     if (account) {
       throw new Error('Já existe uma conta com esse username.')
     }
+    await this.encrypter.generate(password)
     return null
   }
 }
