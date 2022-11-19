5d2d6e6e62779f6a5b9cd3f0ddfd52b62dbeefd0
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-19 17:25:26 -0300

    test: ensure CreateAccountUseCase calls Encrypter with correct values

--- src/data/usecases/account/create-account/create-account-use-case.spec.ts
+++ src/data/usecases/account/create-account/create-account-use-case.spec.ts
@@ -1,6 +1,7 @@
 import { AccountModel } from '../../../../domain/models/account'
 import { CreateAccountModel } from '../../../../domain/usecases/account/create-account'
 import { LoadAccountByUsernameRepository } from '../../../protocols/db/account/load-account-by-username-repository'
+import { Encrypter } from '../../../protocols/criptography/encrypter'
 import { CreateAccountUseCase } from './create-account-use-case'
 
 const makeAccountModel = (): AccountModel => ({
@@ -30,18 +31,33 @@ const makeLoadAccountByUsernameRepositoryStub =
     return new LoadAccountByUsernameRepositoryStub()
   }
 
+const makeEncrypterStub = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async generate(value: string): Promise<string> {
+      return 'hashed_password'
+    }
+  }
+  return new EncrypterStub()
+}
+
 type SutTypes = {
   sut: CreateAccountUseCase
   loadAccountByUsernameRepositoryStub: LoadAccountByUsernameRepository
+  encrypterStub: Encrypter
 }
 
 const makeSut = (): SutTypes => {
   const loadAccountByUsernameRepositoryStub =
     makeLoadAccountByUsernameRepositoryStub()
-  const sut = new CreateAccountUseCase(loadAccountByUsernameRepositoryStub)
+  const encrypterStub = makeEncrypterStub()
+  const sut = new CreateAccountUseCase(
+    loadAccountByUsernameRepositoryStub,
+    encrypterStub
+  )
   return {
     sut,
-    loadAccountByUsernameRepositoryStub
+    loadAccountByUsernameRepositoryStub,
+    encrypterStub
   }
 }
 
@@ -75,6 +91,15 @@ describe('CreateAccountUseCase', () => {
     const promise = sut.execute(makeCreateAccountRequest())
     await expect(promise).rejects.toThrow(
       new Error('Já existe uma conta com esse username.')
+    )
+  })
+
+  test('Should call Encrypter with correct values', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encrypterSpy = jest.spyOn(encrypterStub, 'generate')
+    await sut.execute(makeCreateAccountRequest())
+    expect(encrypterSpy).toHaveBeenCalledWith(
+      makeCreateAccountRequest().password
     )
   })
 })
