8721fe7d3dffe310db4ca5c03bb871aa5c6b121d
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-22 23:27:00 -0300

    test: ensure AccountMongoRepository creates a new account on success

---
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -0,0 +1,41 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { AccountMongoRepository } from './account-mongo-repository'
+
+let accountCollection: Collection
+
+const makeCreateAccountRequest = () => ({
+  username: 'any_username',
+  password: 'hashed_password',
+  isAdmin: false
+})
+describe('AccountMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
+  describe('create()', () => {
+    test('Should create a new account on success', async () => {
+      const sut = makeSut()
+      const accountData = await sut.create(makeCreateAccountRequest())
+      expect(accountData).toBeTruthy()
+      expect(accountData?.id).toBeTruthy()
+      expect(accountData?.username).toEqual('any_username')
+      expect(accountData?.password).toEqual('hashed_password')
+      expect(accountData?.isAdmin).toBeFalsy()
+    })
+  })
+})
