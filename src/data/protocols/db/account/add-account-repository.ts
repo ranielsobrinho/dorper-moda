9eebaaeabafef5e4fdd4594897e9826de2097a16
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-22 22:33:15 -0300

    feat: ensure CreateAccountUseCase calls AddAccountRepository with correct values

---
+++ src/data/protocols/db/account/add-account-repository.ts
@@ -0,0 +1,13 @@
+import { AccountModel } from '../../../../domain/models/account'
+import { CreateAccountModel } from '../../../../domain/usecases/account/create-account'
+
+export interface AddAccountRepository {
+  create(
+    params: AddAccountRepository.Params
+  ): Promise<AddAccountRepository.Result>
+}
+
+export namespace AddAccountRepository {
+  export type Params = CreateAccountModel
+  export type Result = AccountModel
+}
--- src/data/usecases/account/create-account/create-account-use-case.ts
+++ src/data/usecases/account/create-account/create-account-use-case.ts
@@ -1,11 +1,13 @@
 import { CreateAccount } from '../../../../domain/usecases/account/create-account'
 import { Encrypter } from '../../../protocols/criptography/encrypter'
+import { AddAccountRepository } from '../../../protocols/db/account/add-account-repository'
 import { LoadAccountByUsernameRepository } from '../../../protocols/db/account/load-account-by-username-repository'
 
 export class CreateAccountUseCase implements CreateAccount {
   constructor(
     private readonly loadAccountByUsernameRepository: LoadAccountByUsernameRepository,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
   ) {}
 
   async execute(params: CreateAccount.Params): Promise<CreateAccount.Result> {
@@ -16,7 +18,10 @@ export class CreateAccountUseCase implements CreateAccount {
     if (account) {
       throw new Error('Já existe uma conta com esse username.')
     }
-    await this.encrypter.generate(password)
+    const hashedPassword = await this.encrypter.generate(password)
+    await this.addAccountRepository.create(
+      Object.assign({}, params, { password: hashedPassword })
+    )
     return null
   }
 }
