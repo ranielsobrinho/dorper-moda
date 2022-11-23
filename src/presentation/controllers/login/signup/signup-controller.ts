fdfe6beb4e2c99a0979e60d302b825591ae8d90e
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-23 00:56:51 -0300

    feat: ensure SignupController returns 500 if CreateAccount throws

--- src/presentation/controllers/login/signup/signup-controller.ts
+++ src/presentation/controllers/login/signup/signup-controller.ts
@@ -1,12 +1,16 @@
 import { CreateAccount } from '../../../../domain/usecases/account/create-account'
-import { noContent } from '../../../helpers/http-helper'
+import { noContent, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class SignupController implements Controller {
   constructor(private readonly createAccountUseCase: CreateAccount) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.createAccountUseCase.execute(httpRequest.body)
-    return noContent()
+    try {
+      await this.createAccountUseCase.execute(httpRequest.body)
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
