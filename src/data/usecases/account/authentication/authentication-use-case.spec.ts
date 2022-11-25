b302236c5f227b20b80fb14b199e0dea1d1776dd
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-25 15:49:35 -0300

    test: ensure AuthenticationUseCase calls TokenGenerator with correct values

--- src/data/usecases/account/authentication/authentication-use-case.spec.ts
+++ src/data/usecases/account/authentication/authentication-use-case.spec.ts
@@ -1,5 +1,6 @@
 import { LoadAccountByUsernameRepository } from '../../../protocols/db/account/load-account-by-username-repository'
 import { HashComparer } from '../../../protocols/criptography/hash-comparer'
+import { TokenGenerator } from '../../../protocols/criptography/token-generator'
 import { AuthenticationModel } from '../../../../domain/usecases/account/authentication'
 import { AuthenticationUseCase } from './authentication-use-case'
 import { AccountModel } from '../../../../domain/models/account'
@@ -28,33 +29,46 @@ const makeLoadAccountByUsernameRepositoryStub =
     return new LoadAccountByUsernameRepositoryStub()
   }
 
-const makeHashComparerRepositoryStub = (): HashComparer => {
-  class HashComparerRepositoryStub implements HashComparer {
+const makeHashComparerStub = (): HashComparer => {
+  class HashComparerStub implements HashComparer {
     async compare(): Promise<boolean> {
       return Promise.resolve(true)
     }
   }
-  return new HashComparerRepositoryStub()
+  return new HashComparerStub()
+}
+
+const makeTokenGeneratorStub = (): TokenGenerator => {
+  class TokenGeneratorStub implements TokenGenerator {
+    async generate(): Promise<string> {
+      return Promise.resolve('any_token')
+    }
+  }
+  return new TokenGeneratorStub()
 }
 
 type SutTypes = {
   sut: AuthenticationUseCase
   loadAccountByUsernameRepositoryStub: LoadAccountByUsernameRepository
   hashComparerStub: HashComparer
+  tokenGeneratorStub: TokenGenerator
 }
 
 const makeSut = (): SutTypes => {
   const loadAccountByUsernameRepositoryStub =
     makeLoadAccountByUsernameRepositoryStub()
-  const hashComparerStub = makeHashComparerRepositoryStub()
+  const hashComparerStub = makeHashComparerStub()
+  const tokenGeneratorStub = makeTokenGeneratorStub()
   const sut = new AuthenticationUseCase(
     loadAccountByUsernameRepositoryStub,
-    hashComparerStub
+    hashComparerStub,
+    tokenGeneratorStub
   )
   return {
     sut,
     loadAccountByUsernameRepositoryStub,
-    hashComparerStub
+    hashComparerStub,
+    tokenGeneratorStub
   }
 }
 
@@ -111,5 +125,12 @@ describe('AuthenticatioUseCase', () => {
     jest.spyOn(hashComparerStub, 'compare').mockResolvedValueOnce(false)
     const accessToken = await sut.auth(makeAuthenticationRequest())
     expect(accessToken).toBeNull()
+  })
+
+  test('Should call TokenGenerator with correct values', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+    const tokenGeneratorSpy = jest.spyOn(tokenGeneratorStub, 'generate')
+    await sut.auth(makeAuthenticationRequest())
+    expect(tokenGeneratorSpy).toHaveBeenCalledWith(makeAccountModel().id)
   })
 })
