325ef82aea50019bea79c6aa0dbb304007a1621b
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-25 15:11:42 -0300

    feat: ensure AuthenticationUseCase calls HashComparer with correct values

--- src/data/usecases/account/authentication/authentication-use-case.ts
+++ src/data/usecases/account/authentication/authentication-use-case.ts
@@ -1,17 +1,20 @@
 import { Authentication } from '../../../../domain/usecases/account/authentication'
+import { HashComparer } from '../../../protocols/criptography/hash-comparer'
 import { LoadAccountByUsernameRepository } from '../../../protocols/db/account/load-account-by-username-repository'
 
 export class AuthenticationUseCase implements Authentication {
   constructor(
-    private readonly loadAccountByUsernameRepository: LoadAccountByUsernameRepository
+    private readonly loadAccountByUsernameRepository: LoadAccountByUsernameRepository,
+    private readonly hashComparer: HashComparer
   ) {}
 
   async auth(params: Authentication.Params): Promise<Authentication.Result> {
-    const { username } = params
+    const { username, password } = params
     const account = await this.loadAccountByUsernameRepository.loadByUsername(
       username
     )
     if (account) {
+      await this.hashComparer.compare(password, account?.password)
       return 'lalala'
     }
     return null
