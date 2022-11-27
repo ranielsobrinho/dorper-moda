389e6ba50395653e3e42ee1799f488876d25bf81
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-27 11:03:20 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -19,5 +19,14 @@ describe('JwtAdapter', () => {
       await sut.generate('any_id')
       expect(jwtAdapterSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
     })
+
+    test('Should throw if sign throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(async () => {
+        return Promise.reject(new Error())
+      })
+      const promise = sut.generate('any_id')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
