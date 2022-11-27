df3ff05345c5d155397e87bf16760426e8121b1d
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-27 11:26:38 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -1,22 +1,42 @@
 import { Authentication } from '../../../../domain/usecases/account/authentication'
+import { HttpRequest } from '../../../protocols'
 import { LoginController } from './login-controller'
+
+const makeAuthenticationStub = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth(): Promise<Authentication.Result> {
+      return Promise.resolve('any_token')
+    }
+  }
+  return new AuthenticationStub()
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    username: 'any_username',
+    password: 'any_password'
+  }
+})
+
+type SutTypes = {
+  sut: LoginController
+  authenticationStub: Authentication
+}
+
+const makeSut = (): SutTypes => {
+  const authenticationStub = makeAuthenticationStub()
+  const sut = new LoginController(authenticationStub)
+  return {
+    sut,
+    authenticationStub
+  }
+}
 
 describe('LoginController', () => {
   test('Should call Authentication with correct values', async () => {
-    class AuthenticationStub implements Authentication {
-      async auth(): Promise<Authentication.Result> {
-        return Promise.resolve('any_token')
-      }
-    }
-    const authenticationStub = new AuthenticationStub()
-    const sut = new LoginController(authenticationStub)
+    const { sut, authenticationStub } = makeSut()
     const authenticationSpy = jest.spyOn(authenticationStub, 'auth')
-    await sut.handle({
-      body: {
-        username: 'any_username',
-        password: 'any_password'
-      }
-    })
+    await sut.handle(makeFakeRequest())
     expect(authenticationSpy).toHaveBeenCalledWith({
       username: 'any_username',
       password: 'any_password'
