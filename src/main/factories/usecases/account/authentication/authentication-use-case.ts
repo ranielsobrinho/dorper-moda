faa49380acce5f73fe101a9b19e85b1e3103c74a
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-28 11:10:22 -0300

    feat: create authentication use case method

---
+++ src/main/factories/usecases/account/authentication/authentication-use-case.ts
@@ -0,0 +1,14 @@
+import { AuthenticationUseCase } from '../../../../../data/usecases/account/authentication/authentication-use-case'
+import { Authentication } from '../../../../../domain/usecases/account/authentication'
+import { BcryptAdapter } from '../../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../../../config/env'
+
+export const makeAuthenticationUseCase = (): Authentication => {
+  const salt = 12
+  const accountRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtToken)
+  return new AuthenticationUseCase(accountRepository, bcryptAdapter, jwtAdapter)
+}
