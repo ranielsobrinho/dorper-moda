69e22f00af968d27d8d59b9515f7e6d50f1393f0
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-28 10:40:36 -0300

    feat: ensure LoginController throws if Authentication throws

--- src/presentation/controllers/login/login/login-controller.ts
+++ src/presentation/controllers/login/login/login-controller.ts
@@ -1,16 +1,24 @@
 import { Authentication } from '../../../../domain/usecases/account/authentication'
-import { noContent, unauthorized } from '../../../helpers/http-helper'
+import {
+  noContent,
+  serverError,
+  unauthorized
+} from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoginController implements Controller {
   constructor(private readonly authentication: Authentication) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { username, password } = httpRequest.body
-    const accessToken = await this.authentication.auth({ username, password })
-    if (!accessToken) {
-      return unauthorized()
+    try {
+      const { username, password } = httpRequest.body
+      const accessToken = await this.authentication.auth({ username, password })
+      if (!accessToken) {
+        return unauthorized()
+      }
+      return noContent()
+    } catch (error) {
+      return serverError(error)
     }
-    return noContent()
   }
 }
