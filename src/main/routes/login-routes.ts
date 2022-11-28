27fe7309ec7c6bf95c1a972ee2bf01b21c3aef62
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-28 11:11:19 -0300

    feat: ensure login route returns 200 if valid credentials are provided

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeCreateAccountController } from '../factories/controllers/account/create-account/create-account-factory'
+import { makeLoginController } from '../factories/controllers/account/login/login-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeCreateAccountController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
