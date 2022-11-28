9bbe94672d0b37594b66a9c6f987ef1db4ac503c
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-28 11:09:55 -0300

    feat: add jwt token env variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   mongoUrl:
-    process.env.MONGO_URL || 'mongodb://localhost:27017/dorper-moda-api',
-  port: process.env.PORT || 5000
+    process.env.MONGO_URL ?? 'mongodb://localhost:27017/dorper-moda-api',
+  port: process.env.PORT ?? 5000,
+  jwtToken: process.env.JWT_TOKEN ?? 'jwtsdaSDSLKdfe512654ç'
 }
