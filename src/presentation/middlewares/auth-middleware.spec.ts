762dd7e70d16886378d91d9b9fdf4452bba771bd
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-29 18:05:27 -0300

    refactor: move sut creation to a factory method

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,19 +1,39 @@
 import { VerifyToken } from '../../domain/usecases/authentication/verify-token'
+import { HttpRequest } from '../protocols'
 import { AuthMiddleware } from './auth-middleware'
+
+const makeRequest = (): HttpRequest => ({
+  headers: {
+    'x-access-token': 'any_token'
+  }
+})
+
+const makeVerifyTokenStub = (): VerifyToken => {
+  class VerifyTokenStub implements VerifyToken {
+    async execute(): Promise<VerifyToken.Result> {}
+  }
+  return new VerifyTokenStub()
+}
+
+type SutTypes = {
+  sut: AuthMiddleware
+  verifyTokenStub: VerifyToken
+}
+
+const makeSut = (): SutTypes => {
+  const verifyTokenStub = makeVerifyTokenStub()
+  const sut = new AuthMiddleware(verifyTokenStub)
+  return {
+    sut,
+    verifyTokenStub
+  }
+}
 
 describe('Auth Middleware', () => {
   test('Should call VerifyToken with correct values', async () => {
-    class VerifyTokenStub implements VerifyToken {
-      async execute(): Promise<VerifyToken.Result> {}
-    }
-    const verifyTokenStub = new VerifyTokenStub()
-    const sut = new AuthMiddleware(verifyTokenStub)
+    const { sut, verifyTokenStub } = makeSut()
     const verifyTokenSpy = jest.spyOn(verifyTokenStub, 'execute')
-    await sut.handle({
-      headers: {
-        'x-access-token': 'any_token'
-      }
-    })
+    await sut.handle(makeRequest())
     expect(verifyTokenSpy).toHaveBeenCalledWith('any_token')
   })
 })
