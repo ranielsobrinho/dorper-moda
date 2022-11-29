96a9de82701f7df6f8fa262b2ef643a4e79db0f2
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-11-29 16:36:59 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/authentication/verify-token/verify-token-use-case.spec.ts
+++ src/data/usecases/authentication/verify-token/verify-token-use-case.spec.ts
@@ -1,15 +1,32 @@
 import { TokenVerify } from '../../../protocols/criptography/token-verify'
 import { VerifyTokenUseCase } from './verify-token-use-case'
 
+const makeDecrypterStub = (): TokenVerify => {
+  class DecrypterStub implements TokenVerify {
+    async verify(): Promise<string> {
+      return Promise.resolve('any_value')
+    }
+  }
+  return new DecrypterStub()
+}
+
+type SutTypes = {
+  sut: VerifyTokenUseCase
+  decrypterStub: TokenVerify
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = makeDecrypterStub()
+  const sut = new VerifyTokenUseCase(decrypterStub)
+  return {
+    sut,
+    decrypterStub
+  }
+}
+
 describe('VerifyTokenUseCase', () => {
   test('Should call Decrypter with correct value', async () => {
-    class DecrypterStub implements TokenVerify {
-      async verify(): Promise<string> {
-        return Promise.resolve('any_value')
-      }
-    }
-    const decrypterStub = new DecrypterStub()
-    const sut = new VerifyTokenUseCase(decrypterStub)
+    const { sut, decrypterStub } = makeSut()
     const verifyTokenSpy = jest.spyOn(decrypterStub, 'verify')
     await sut.execute('any_token')
     expect(verifyTokenSpy).toHaveBeenCalledWith('any_token')
