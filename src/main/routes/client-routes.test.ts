a2ce56bd1f3629c286d44fd4f95af48c94a7d995
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2023-04-20 10:34:57 -0300

    test: ensure create client route api is running

---
+++ src/main/routes/client-routes.test.ts
@@ -0,0 +1,100 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import request from 'supertest'
+import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+
+const makeAccessToken = async (): Promise<string> => {
+  const account = await accountCollection.insertOne({
+    username: 'any_name',
+    password: 'hashed_password',
+    isAdmin: false
+  })
+  return sign(account.insertedId.toString(), env.jwtToken)
+}
+
+let clientsCollection: Collection
+let accountCollection: Collection
+describe('Sales Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    clientsCollection = MongoHelper.getCollection('clients')
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+    await clientsCollection.deleteMany({})
+  })
+
+  describe('POST /clients', () => {
+    test('Should return 204 on success', async () => {
+      const accessToken = await makeAccessToken()
+      await request(app)
+        .post('/api/clients')
+        .set('x-access-token', accessToken)
+        .send({
+          name: 'any_name',
+          address: 'any_address',
+          cpf: 'any_cpf',
+          telephone: 'any_telephone',
+          baseFee: 10
+        })
+        .expect(200)
+    })
+
+    test('Should return 400 if missing a required field', async () => {
+      const accessToken = await makeAccessToken()
+      await request(app)
+        .post('/api/clients')
+        .set('x-access-token', accessToken)
+        .send({
+          name: 'any_name',
+          address: 'any_address',
+          cpf: 'any_cpf',
+          telephone: 'any_telephone'
+        })
+        .expect(400)
+    })
+
+    test('Should return 403 if is missing access token', async () => {
+      await request(app)
+        .post('/api/clients')
+        .send({
+          name: 'any_name',
+          address: 'any_address',
+          cpf: 'any_cpf',
+          telephone: 'any_telephone',
+          baseFee: 10
+        })
+        .expect(403)
+    })
+
+    test('Should return 400 if cpf is already taken', async () => {
+      const accessToken = await makeAccessToken()
+      await clientsCollection.insertOne({
+        name: 'any_name',
+        address: 'any_address',
+        cpf: 'any_cpf',
+        telephone: 'any_telephone',
+        baseFee: 10
+      })
+      await request(app)
+        .post('/api/clients')
+        .set('x-access-token', accessToken)
+        .send({
+          name: 'any_name',
+          address: 'any_address',
+          cpf: 'any_cpf',
+          telephone: 'any_telephone',
+          baseFee: 10
+        })
+        .expect(400)
+    })
+  })
+})
