9860d608f39bc65a592fc2aa6efffeb5ef118b3a
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2023-04-21 09:59:12 -0300

    feat: ensure ClientsMongoRepository deletes a client on success

--- src/infra/db/mongodb/clients/clients-mongo-repository.ts
+++ src/infra/db/mongodb/clients/clients-mongo-repository.ts
@@ -2,12 +2,14 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { CreateClientRepository } from '../../../../data/protocols/db/clients/create-client-repository'
 import { GetClientByCpfRepository } from '../../../../data/protocols/db/clients/get-client-by-cpf-repository'
 import { GetClientsRepository } from '../../../../data/protocols/db/clients/get-clients-repository'
+import { DeleteClientRepository } from '../../../../data/protocols/db/clients/delete-client-repository'
 
 export class ClientsMongoRepository
   implements
     CreateClientRepository,
     GetClientByCpfRepository,
-    GetClientsRepository
+    GetClientsRepository,
+    DeleteClientRepository
 {
   async create(
     params: CreateClientRepository.Params
@@ -30,5 +32,14 @@ export class ClientsMongoRepository
     const clientsCollection = MongoHelper.getCollection('clients')
     const clientData = await clientsCollection.find().toArray()
     return clientData && MongoHelper.mapCollection(clientData)
+  }
+
+  async delete(cpf: string): Promise<DeleteClientRepository.Result> {
+    const clientsCollection = MongoHelper.getCollection('clients')
+    const clientData = await clientsCollection.deleteOne({ cpf })
+    if (clientData.deletedCount > 0) {
+      return 'OK'
+    }
+    return null
   }
 }
